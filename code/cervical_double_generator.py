66a8b10feafe2bf306b071d7dcf4ebbf78b28931
Kamil Adamczewski <kamil.m.adamczewski@gmail.com>
2020-01-14 10:29:47 +0100

    organizing code

--- code/cervical_double_generator.py
+++ code/cervical_double_generator.py
@@ -34,8 +34,9 @@ if user=='mijung':
 elif user =='kamil':
     Results_PATH = "/home/kamil/Desktop/Dropbox/Current_research/privacy/DPDR/results/separate_isolet"
 
-#X - data
-#W  - random samples for generation
+        #n_features - random Fourier features
+        #X - real/generated data
+        #W - some random features (half of n_features)
 def RFF_Gauss(n_features, X, W):
     """ this is a Pytorch version of Wittawat's code for RFFKGauss"""
     # Fourier transform formula from
@@ -83,6 +84,13 @@ def main(features_num, batch_cl0,  input_cl0, hidden1_cl0, hidden2_cl0, epochs_n
     #input_dim - number of input features in data
     #n  - number of samples
 
+    ######################3
+
+
+
+
+    ###############3
+
     random.seed(0)
 
 
@@ -93,7 +101,7 @@ def main(features_num, batch_cl0,  input_cl0, hidden1_cl0, hidden2_cl0, epochs_n
         data_nan=pd.read_csv("/home/kadamczewski/Dropbox_from/Current_research/privacy/DPDR/data/Cervical/kag_risk_factors_cervical_cancer.csv")
 
 
-    preprocessing='removald'
+    preprocessing='removal'
 
     numerical_df = ['Age', 'Numberofsexualpartners', 'Firstsexualintercourse', 'Numofpregnancies', 'Smokes(years)',
                         'Smokes(packs/year)', 'HormonalContraceptives(years)', 'IUD(years)', 'STDs(number)'
@@ -114,7 +122,7 @@ def main(features_num, batch_cl0,  input_cl0, hidden1_cl0, hidden2_cl0, epochs_n
 
 
     #######################################
-    if preprocessing=='removald':
+    if preprocessing=='removal':
 
         df1 = data_nan.convert_objects(convert_numeric=True)
         #df1=pd.to_numeric(data_nan)
@@ -187,198 +195,189 @@ def main(features_num, batch_cl0,  input_cl0, hidden1_cl0, hidden2_cl0, epochs_n
     #     #print(data_target.at[i,'y'])
     #     data_target.at[i,'y'] = 0
 
-    ##################################################3
+    ###################################################################################################################3
 
     #optionsfor data_features:
     # data - all
     # data_categorical
     # data_numerical
 
-    data_features=data_numerical
+    print("*" * 100)
+    for data_used in ["all", "numerical", "categorical"]:
 
-    X_train, X_test, y_train, y_test = train_test_split(data_features, data_target, train_size=0.80, test_size=0.20,
-                                                        random_state=0)
+        if data_used=="all":
+            data_features=data
+        elif data_used=="numerical":
+            data_features=data_numerical
+        elif data_used=="categorical":
+            data_features=data_categorical
 
-    # test logistic regression on the real data
-    LR_model = LogisticRegression(solver='lbfgs', max_iter=1000)
-    #LR_model = DecisionTreeClassifier()
+        print("data used: " , data_used)
 
-    LR_model.fit(X_train, y_train.values.ravel())  # training on synthetic data
-    pred = LR_model.predict(X_test)  # test on real data
 
-    print('ROC on real test data is', roc_auc_score(y_test, pred))
-    print('PRC on real test data is', average_precision_score(y_test, pred))
+        X_train, X_test, y_train, y_test = train_test_split(data_features, data_target, train_size=0.80, test_size=0.20,
+                                                            random_state=0)
 
-    y_labels = y_train.values.ravel()  # X_train_pos
-    X_train = X_train.values
+        # test logistic regression on the real data
+        LR_model = LogisticRegression(solver='lbfgs', max_iter=1000)
+        #LR_model = DecisionTreeClassifier()
 
-    n_tot = X_train.shape[0]
+        LR_model.fit(X_train, y_train.values.ravel())  # training on synthetic data
+        pred = LR_model.predict(X_test)  # test on real data
 
-    X_train_pos = X_train[y_labels == 1, :]
-    y_train_pos = y_labels[y_labels == 1]
+        print('ROC on real test data is', roc_auc_score(y_test, pred))
+        print('PRC on real test data is', average_precision_score(y_test, pred), '\n')
 
-    X_train_neg = X_train[y_labels == 0, :]
-    y_train_neg = y_labels[y_labels == 0]
+        y_labels = y_train.values.ravel()  # X_train_pos
+        X_train = X_train.values
 
-    ###############################################################3
-    # train generator
+        n_tot = X_train.shape[0]
 
-    n_classes = 2
+        X_train_pos = X_train[y_labels == 1, :]
+        y_train_pos = y_labels[y_labels == 1]
 
-    for which_class in range(n_classes):
+        X_train_neg = X_train[y_labels == 0, :]
+        y_train_neg = y_labels[y_labels == 0]
 
-        if which_class == 1:
-            """ First train for positive label """
-            n, input_dim = X_train_pos.shape
-            data_samps = X_train_pos
-            del X_train_pos
-        else:
-            n, input_dim = X_train_neg.shape
-            data_samps = X_train_neg
-            del X_train_neg
+        ###############################################################3
+        # train generator
 
-        # test how to use RFF for computing the kernel matrix
-        idx_rp = np.random.permutation(np.min([n, 10000]))
-        med = util.meddistance(data_samps[idx_rp, :])
-        del idx_rp
-        sigma2 = med ** 2
-        print('length scale from median heuristic is', sigma2)
+        n_classes = 2
 
-        # random Fourier features
-        n_features = features_num  # 20000
+        for which_class in range(n_classes):
 
-        """ training a Generator via minimizing MMD """
-        # try more random features with a larger batch size
+            print("Class: ", which_class)
 
-        # mini_batch_size = n
-        #
-        # input_size = np.int(inp * input_dim)
-        # hidden_size_1 = h1 * input_dim
-        # hidden_size_2 = h2 * input_dim
-        # output_size = input_dim
+            if which_class == 1:
+                # First train for positive label
+                n, input_dim = X_train_pos.shape
+                data_samps = X_train_pos
+                del X_train_pos
+            else:
+                n, input_dim = X_train_neg.shape
+                data_samps = X_train_neg
+                del X_train_neg
 
-        # doesn't work
-        # if which_class == 1:
-        #
-        #     mini_batch_size = 200
-        #     input_size = 40
-        #     hidden_size_1 = 200
-        #     hidden_size_2 = 100
-        #     output_size = input_dim
-        #     how_many_epochs = 1000
-        #
-        # else:  # for extremely imbalanced dataset
-        #     mini_batch_size = 4000  # large minibatch size for speeding up the training process
-        #     input_size = 100
-        #     hidden_size_1 = 500
-        #     hidden_size_2 = 200
-        #     output_size = input_dim
-        #     how_many_epochs = 100  # 400
 
-        if which_class == 1:
+            ##################
 
-            mini_batch_size = batch_cl1  # 400
-            input_size = input_cl1  # 400
-            hidden_size_1 = hidden1_cl1  # 100
-            hidden_size_2 = hidden2_cl1  # 100
-            output_size = input_dim
-            how_many_epochs = epochs_num_cl1  # 30
+            #training a Generator via minimizing MMD
+            # try more random features with a larger batch size
 
-        else:  # for extremely imbalanced dataset
+            if which_class == 1:
 
-            mini_batch_size = batch_cl0  # 400
-            input_size = input_cl0  # 400
-            hidden_size_1 = hidden1_cl0  # 300
-            hidden_size_2 = hidden2_cl0  # 100
+                mini_batch_size = batch_cl1  # 400
+                input_size = input_cl1  # 400
+                hidden_size_1 = hidden1_cl1  # 100
+                hidden_size_2 = hidden2_cl1  # 100
+                output_size = input_dim
+                how_many_epochs = epochs_num_cl1  # 30
 
-            output_size = input_dim
-            how_many_epochs = epochs_num_cl0  # 30
+            else:  # for extremely imbalanced dataset
 
-            # mini_batch_size = 4000 # large minibatch size for speeding up the training process
-            # input_size = 100
-            # hidden_size_1 = 500
-            # hidden_size_2 = 200
-            # output_size = input_dim
-            # how_many_epochs = 400
+                mini_batch_size = batch_cl0  # 400
+                input_size = input_cl0  # 400
+                hidden_size_1 = hidden1_cl0  # 300
+                hidden_size_2 = hidden2_cl0  # 100
 
-        output_size = input_dim
+                output_size = input_dim
+                how_many_epochs = epochs_num_cl0  # 30
 
-        model = Generative_Model(input_size=input_size, hidden_size_1=hidden_size_1, hidden_size_2=hidden_size_2,
-                                 output_size=output_size)
+            ############
 
-        optimizer = optim.Adam(model.parameters(), lr=1e-3)
-        how_many_iter = np.int(n / mini_batch_size)
+            # test how to use RFF for computing the kernel matrix
+            # same for the real and generated data
+            idx_rp = np.random.permutation(np.min([n, 10000]))
+            med = util.meddistance(data_samps[idx_rp, :])
+            del idx_rp
+            sigma2 = med ** 2
+            print('length scale from median heuristic is', sigma2)
 
-        training_loss_per_epoch = np.zeros(how_many_epochs)
+            # random Fourier features
+            n_features = features_num  # 20000
 
-        draws = n_features // 2
-        W_freq = np.random.randn(draws, input_dim) / np.sqrt(sigma2)
 
-        """ computing mean embedding of true data """
-        emb1_input_features = RFF_Gauss(n_features, torch.Tensor(data_samps), W_freq)
-        mean_emb1 = torch.mean(emb1_input_features, 0)
+            draws = n_features // 2
+            W_freq = np.random.randn(draws, input_dim) / np.sqrt(sigma2)
 
-        del data_samps
-        del emb1_input_features
+            #####
 
-        # plt.plot(mean_emb1, 'b')
+            """ computing mean embedding of true data """
+            #n_features - random Fourier features, e.g. 400
+            #data_samps - real/generated data, e.g. 498x11
+            #W_freq - some random features (half of n_features), e.g [400, 498]
+            emb1_input_features = RFF_Gauss(n_features, torch.Tensor(data_samps), W_freq)
+            mean_emb1 = torch.mean(emb1_input_features, 0) #e.g. [400]
 
-        ############################################################33
-        # start training
+            del data_samps
+            del emb1_input_features
 
-        print('Starting Training')
+            # plt.plot(mean_emb1, 'b')
 
-        for epoch in range(how_many_epochs):  # loop over the dataset multiple times
+            ############################################################33
+            # start training
 
-            running_loss = 0.0
-            # annealing_rate =
+            model = Generative_Model(input_size=input_size, hidden_size_1=hidden_size_1, hidden_size_2=hidden_size_2,
+                                     output_size=output_size)
 
-            for i in range(how_many_iter):
-                # zero the parameter gradients
-                optimizer.zero_grad()
-                input_to_model = torch.randn((mini_batch_size, input_size))
-                outputs = model(input_to_model)
+            optimizer = optim.Adam(model.parameters(), lr=1e-3)
+            how_many_iter = np.int(n / mini_batch_size)
 
-                """ computing mean embedding of generated samples """
-                emb2_input_features = RFF_Gauss(n_features, outputs, W_freq)
-                mean_emb2 = torch.mean(emb2_input_features, 0)
+            training_loss_per_epoch = np.zeros(how_many_epochs)
 
-                loss = torch.norm(mean_emb1 - mean_emb2, p=2) ** 2
+            print('Starting Training')
 
-                loss.backward()
-                optimizer.step()
+            for epoch in range(how_many_epochs):  # loop over the dataset multiple times
 
-                # print statistics
-                running_loss += loss.item()
+                running_loss = 0.0
+                # annealing_rate =
 
-            #print('epoch # and running loss are ', [epoch, running_loss])
-            training_loss_per_epoch[epoch] = running_loss
+                for i in range(how_many_iter):
+                    # zero the parameter gradients
+                    optimizer.zero_grad()
+                    input_to_model = torch.randn((mini_batch_size, input_size))
+                    outputs = model(input_to_model)
 
-        #################################3
-        # generate samples
+                    """ computing mean embedding of generated samples """
+                    emb2_input_features = RFF_Gauss(n_features, outputs, W_freq)
+                    mean_emb2 = torch.mean(emb2_input_features, 0)
 
-        """ now generated samples using the trained generator """
-        input_to_model = torch.randn((n, input_size))
-        outputs = model(input_to_model)
-        samp_input_features = outputs
+                    loss = torch.norm(mean_emb1 - mean_emb2, p=2) ** 2
 
-        if which_class == 1:
-            generated_samples_pos = samp_input_features.detach().numpy()
+                    loss.backward()
+                    optimizer.step()
 
-            # save results
-            method = os.path.join(Results_PATH, 'Isolet_pos_samps_batch_size=%s_input_size=%s_hidden1=%s_hidden2=%s' % (
-                mini_batch_size, input_size, hidden_size_1, hidden_size_2))
-            # np.save(method + '_loss.npy', training_loss_per_epoch)
-            # np.save(method + '_input_feature_samps.npy', generated_samples_pos)
+                    # print statistics
+                    running_loss += loss.item()
 
-        else:
-            generated_samples_neg = samp_input_features.detach().numpy()
+                #print('epoch # and running loss are ', [epoch, running_loss])
+                training_loss_per_epoch[epoch] = running_loss
 
-            # save results
-            method = os.path.join(Results_PATH, 'Isolet_neg_samps_batch_size=%s_input_size=%s_hidden1=%s_hidden2=%s' % (
-                mini_batch_size, input_size, hidden_size_1, hidden_size_2))
-            # np.save(method + '_loss.npy', training_loss_per_epoch)
-            # np.save(method + '_input_feature_samps.npy', generated_samples_neg)
+            #################################3
+            # generate samples
+
+            """ now generated samples using the trained generator """
+            input_to_model = torch.randn((n, input_size))
+            outputs = model(input_to_model)
+            samp_input_features = outputs
+
+            if which_class == 1:
+                generated_samples_pos = samp_input_features.detach().numpy()
+
+                # save results
+                method = os.path.join(Results_PATH, 'Isolet_pos_samps_batch_size=%s_input_size=%s_hidden1=%s_hidden2=%s' % (
+                    mini_batch_size, input_size, hidden_size_1, hidden_size_2))
+                # np.save(method + '_loss.npy', training_loss_per_epoch)
+                # np.save(method + '_input_feature_samps.npy', generated_samples_pos)
+
+            else:
+                generated_samples_neg = samp_input_features.detach().numpy()
+
+                # save results
+                method = os.path.join(Results_PATH, 'Isolet_neg_samps_batch_size=%s_input_size=%s_hidden1=%s_hidden2=%s' % (
+                    mini_batch_size, input_size, hidden_size_1, hidden_size_2))
+                # np.save(method + '_loss.npy', training_loss_per_epoch)
+                # np.save(method + '_input_feature_samps.npy', generated_samples_neg)
 
         # plt.figure(3)
         # plt.plot(training_loss_per_epoch)
@@ -388,7 +387,9 @@ def main(features_num, batch_cl0,  input_cl0, hidden1_cl0, hidden2_cl0, epochs_n
         # plt.figure(4)
         # plt.plot(mean_emb1, 'b')
         # plt.plot(mean_emb2.detach().numpy(), 'r--')
+        print("*" * 100)
 
+    print("\nall generated data: ")
     # mix data for positive and negative labels
     generated_input_features = np.concatenate((generated_samples_pos, generated_samples_neg), axis=0)
     corresponding_labels = np.concatenate((y_train_pos, y_train_neg))
