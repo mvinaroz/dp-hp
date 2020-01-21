3464110bbb4b2af8192b3383e375aff12cf38583
Kamil Adamczewski <kamil.m.adamczewski@gmail.com>
2020-01-21 19:44:57 +0100

    minor

--- code/test_condMMD_covtype.py
+++ code/test_condMMD_covtype.py
@@ -333,9 +333,9 @@ def main(n_features_arg, mini_batch_arg, how_many_epochs_arg):
 
 if __name__ == '__main__':
 
-    how_many_epochs_arg=[1000]
-    n_features_arg = [5000, 10000, 15000]
-    mini_batch_arg = [500, 1000]
+    how_many_epochs_arg=[2000]
+    n_features_arg = [20000, 15000]
+    mini_batch_arg = [1000, 500]
     grid = ParameterGrid({"n_features_arg": n_features_arg, "mini_batch_arg": mini_batch_arg, "how_many_epochs_arg": how_many_epochs_arg})
     for elem in grid:
         print(elem)
