8c2210518a69d70f11455c83ab5780c39dc62993
Kamil Adamczewski <kamil.m.adamczewski@gmail.com>
2020-01-21 15:26:36 +0100

    minor

---
+++ git_push.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+git add code/test_condMMD_Isolet.py
+git commit -m "minor"
+git push origin master
--- git_push2.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-git add code/Credit_Single_Generator.py
-git add code/Isolet_Single_Generator.py
-git commit -m "minor"
-git push origin master
