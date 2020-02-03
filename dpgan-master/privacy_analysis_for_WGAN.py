d64ec0e785bd4b421ad0c63e9d805eaebcc58acc
MijungTheGatsbyPostdoc <mpark@lesum.is.localnet>
2020-02-03 16:07:29 +0100

    added d_iters

--- dpgan-master/privacy_analysis_for_WGAN.py
+++ dpgan-master/privacy_analysis_for_WGAN.py
@@ -11,9 +11,10 @@ from autodp import privacy_calibrator
 """" modify these for your dataset """
 #########################################
 total_num_datapt = 50000
-mini_batch_size = 200
+mini_batch_size = 64
 num_epochs = 1000
 num_classes = 2 # how many classes do you consider?  how many generators do you plan to train?
+d_iters = 5 # number of iterations per discriminator update
 #########################################
 
 # Privacy level we set to use
@@ -23,7 +24,7 @@ delta = 1e-5
 # input arguments to autodp
 gamma = mini_batch_size / total_num_datapt # sampling probability
 steps_per_epoch = total_num_datapt // mini_batch_size
-n_steps = steps_per_epoch * num_epochs * num_classes
+n_steps = steps_per_epoch * num_epochs * num_classes * d_iters
 
 print('gamma', gamma)
 print('steps_per_epoch', steps_per_epoch)
