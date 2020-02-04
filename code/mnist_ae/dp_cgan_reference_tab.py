bd84539a2b2ca6240ae6718d34d0d071b86533c0
Kamil Adamczewski <kamil.m.adamczewski@gmail.com>
2020-02-04 17:42:51 +0100

    minor

--- code/mnist_ae/dp_cgan_reference_tab.py
+++ code/mnist_ae/dp_cgan_reference_tab.py
@@ -491,7 +491,7 @@ def main():
     sigma_clipping_list = [[1.12, 1.1]]
     # sigma_clipping_list = [[0.1, 1.1]]
 
-    epsilon = 0.6#9.6
+    epsilon = 9.6#9.6
     # epsilon = 1e10
     delta = 1e-5
 
