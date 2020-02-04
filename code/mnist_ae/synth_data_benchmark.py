7eb0ff2bab42b7be5d1c951738963eee388efefc
MijungTheGatsbyPostdoc <mpark@lesum.is.localnet>
2020-02-04 16:02:06 +0100

    remove l1 penalty for linear svm

--- code/mnist_ae/synth_data_benchmark.py
+++ code/mnist_ae/synth_data_benchmark.py
@@ -85,7 +85,7 @@ def main():
   model_specs = defaultdict(dict)
   model_specs['logistic_reg'] = {'solver': 'lbfgs', 'max_iter': 5000, 'multi_class': 'auto'}
   model_specs['random_forest'] = {'n_estimators': 100, 'class_weight': 'balanced'}
-  model_specs['linear_svc'] = {'max_iter': 5000, 'penalty':'l1', 'tol':1e-8}  # still not enough??
+  model_specs['linear_svc'] = {'max_iter': 5000, 'tol':1e-8, 'loss':'hinge'}  # still not enough??
   model_specs['bernoulli_nb'] = {'binarize': 0.5}
   model_specs['lda'] = {'solver': 'eigen', 'n_components': 9, 'tol': 1e-8, 'shrinkage': 0.5}
   model_specs['decision_tree'] = {'class_weight': 'balanced', 'criterion':'gini', 'splitter':'best', 'min_samples_split':2, 'min_samples_leaf':1, 'min_weight_fraction_leaf':0.0, 'min_impurity_decrease':0.0}
