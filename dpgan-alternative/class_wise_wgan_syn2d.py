7d67deb68c3cd290fd15366dcb7f1f213f56109f
frhrdr <frederikharder@gmail.com>
2020-10-10 13:57:20 +0200

    gathering results

--- dpgan-alternative/class_wise_wgan_syn2d.py
+++ dpgan-alternative/class_wise_wgan_syn2d.py
@@ -32,7 +32,7 @@ def parse_arguments():
   parser.add_argument("--latent-dim", type=int, default=100, help="dimensionality of the latent space")
   parser.add_argument("--n-critic", type=int, default=5, help="number of training steps for discriminator per iter")
 
-  parser.add_argument('--synth-spec-string', type=str, default='disc_k5_n100000_row5_col5_noise0.2', help='')
+  parser.add_argument('--synth-spec-string', type=str, default='norm_k5_n100000_row5_col5_noise0.2', help='')
   parser.add_argument("--clip-value", type=float, default=0.01, help="lower and upper clip value for disc. weights")
   parser.add_argument("--sample-interval", type=int, default=1000, help="interval betwen image samples")
   parser.add_argument("--print-interval", type=int, default=50, help="interval betwen image samples")
