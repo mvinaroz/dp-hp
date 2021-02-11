2ee621501fe65d3404cbe5b99152f9ee40fca695
Kamil <kamil.m.adamczewski@gmail.com>
2021-02-11 23:24:17 +0100

    no make_layers

--- using_pruned_net/VGG16_model.py
+++ using_pruned_net/VGG16_model.py
@@ -98,17 +98,3 @@ class VGG(nn.Module):
 
         return output
 
-    def _make_layers(self, cfg):
-        layers = []
-        in_channels = 3
-        for x in cfg:
-            if x == 'M':
-                layers += [nn.MaxPool2d(kernel_size=2, stride=2)]
-            else:
-                layers += [nn.Conv2d(in_channels, x, kernel_size=3, padding=1),
-                           nn.BatchNorm2d(x),
-                           nn.ReLU(inplace=True)]
-                in_channels = x
-        layers += [nn.AvgPool2d(kernel_size=1, stride=1)]
-        return nn.Sequential(*layers)
-
