d44bc4f757552e73d71f407088a196f4995ccf9d
MijungTheGatsbyPostdoc <mpark@lesum.is.localnet>
2021-03-01 10:26:39 +0100

    test code

--- using_pruned_net/train_gen.py
+++ using_pruned_net/train_gen.py
@@ -7,9 +7,8 @@ import torch
 from test_transfer_learning import load_trained_model, data_loader
 from torch.optim.lr_scheduler import StepLR
 import sys
-# sys.path.append('/home/mpark/DPDR/code_balanced')
 sys.path.append('/is/ei/mpark/DPDR/code_balanced')
-from models_gen import FCCondGen
+from models_gen import FCCondGen, ConvCondGen
 from aux import meddistance
 from full_mmd import mmd_loss
 from aux import flatten_features
@@ -23,10 +22,10 @@ from synth_data_benchmark import test_gen_data, test_passed_gen_data, datasets_c
 
 def main():
 
-    data_name = 'digits' # 'digits' or 'fashion'
+    data_name = 'fashion' # 'digits' or 'fashion'
     model_name = 'ResNet'
     transfer = True
-    reg_L2norm = 0.005 # regularization constant for L2 norm constraint
+    reg_L2norm = 7*1e-5 # regularization constant for L2 norm constraint
     print('%s dataset with regularization constant %f' % (data_name, reg_L2norm))
 
     device = 'cuda' if torch.cuda.is_available() else 'cpu'
@@ -46,8 +45,15 @@ def main():
     input_size = 5 # dimension of z
     feature_dim = 784
     n_classes = 10
-    n_epochs = 50
-    model = FCCondGen(input_size, '500,500', feature_dim, n_classes, use_sigmoid=True, batch_norm=True).to(device)
+    n_epochs = 40
+    # model = FCCondGen(input_size, '500,500', feature_dim, n_classes, use_sigmoid=False, batch_norm=True).to(device)
+    model = ConvCondGen(input_size, '500,500', n_classes, '16,8', '5,5', use_sigmoid=True, batch_norm=True).to(device)
+    # with use_sigmoid=True and reg_L2norm = 5*1e-5 and n_epochs = 10, 66 percent test accuracy on logistic regression.
+    # with use_sigmoid=False and reg_L2norm = 5*1e-5 and n_epochs = 10, 61 percent
+    # with use_sigmoid=True and reg_L2norm = 0 and n_epochs = 10, 50 percent
+    # with use_sigmoid=True and reg_L2norm = 0.0001 and n_epochs = 10, 77 percent test accuracy on logistic regression
+    # with use_sigmoid=True and reg_L2norm = 0.0001 (reg_L2norm=0.00007) and n_epochs = 20, 78 percent test accuracy on logistic regression
+    # # with use_sigmoid=True and reg_L2norm = 0.0005 and n_epochs = 10, 75 percent test accuracy on logistic regression
 
     """ 4. Training """
     # set the scale length
@@ -55,8 +61,12 @@ def main():
     sigma2_arr = np.zeros(np.int(num_iter))
     for batch_idx, (data, labels) in enumerate(data_pkg.train_loader):
         data, labels = data.to(device), labels.to(device)
-        data = flatten_features(data)
-        med = meddistance(data.detach().cpu().numpy())
+        if transfer:
+            _, data_feat = features(data.repeat(1, 3, 1, 1))
+            med = meddistance(data_feat.detach().cpu().numpy())
+        else:
+            data = flatten_features(data)
+            med = meddistance(data.detach().cpu().numpy())
         sigma2 = med**2
         sigma2_arr[batch_idx] = sigma2
         # print(sigma2)
@@ -69,29 +79,49 @@ def main():
         os.makedirs(log_dir)
 
     sigma2 = torch.tensor(np.mean(sigma2_arr))
+    print('length scale', sigma2)
     optimizer = torch.optim.Adam(list(model.parameters()), lr=0.001)
     # print(list(model.parameters()))
     # print('parameters before training')
     # print(list(features.parameters()))
 
     scheduler = StepLR(optimizer, step_size=1, gamma=0.9)
+    # mnist_mean = 0.1307
+    # mnist_sdev = 0.3081
 
     for epoch in range(1, n_epochs + 1):
         for batch_idx, (data, labels) in enumerate(data_pkg.train_loader):
             data, labels = data.to(device), labels.to(device)
             gen_code, gen_labels = model.get_code(batch_size, device)
-            gen_samples = model(gen_code)
+            gen_samples = model(gen_code) # batch_size by 784
+
+            # print('mean before', torch.mean(gen_samples[0,:]))
+            # print('std before', torch.std(gen_samples[0,:]))
+
+            # print('mean data', torch.mean(data[0,:,:,:]))
+            # print('std data', torch.std(data[0,:,:,:]))
+
+            # gen_samples = (gen_samples - torch.mean(gen_samples))/ torch.std(gen_samples)
+            # gen_samples = gen_samples*mnist_sdev + mnist_mean
+
+            # print('mean after', torch.mean(gen_samples[0,:]))
+            # print('std after', torch.std(gen_samples[0,:]))
+
 
             if transfer:
                 # print(gen_samples.shape)
                 # print(data.shape)
+
+                L2norm = torch.mean(torch.sum(gen_samples ** 2, 1).sqrt())  # squared sum across features, then average over datapoints
+                # print('L2norm', L2norm)
+
                 gen_samples = torch.reshape(gen_samples, (batch_size, 1, 28, 28))
                 # print(gen_samples.shape) # minibatch by 784
                 _, data_feat = features(data.repeat(1,3,1,1))
                 _, syn_data_feat = features(gen_samples.repeat(1,3,1,1))
                 # print(data_feat.shape)
                 # print(syn_data_feat.shape)
-                L2norm = torch.mean(torch.sum(gen_samples**2,1).sqrt()) # squared sum across features, then average over datapoints
+
                 # print(L2norm)
                 loss = mmd_loss(data_feat, labels, syn_data_feat, gen_labels, n_classes, sigma2) + reg_L2norm*L2norm
                 # print(loss)
@@ -109,11 +139,32 @@ def main():
             # if batch_idx==0:
             #     break
 
+        # print('mean', torch.mean(gen_samples))
+        # print('std', torch.std(gen_samples))
+
         n_data = len(data_pkg.train_loader.dataset)
         print('Train Epoch: {} [{}/{}]\tLoss: {:.6f}'.format(epoch, batch_idx * len(data), n_data, loss.item()))
 
         log_gen_data(model, device, epoch, data_pkg.n_labels, log_dir)
         scheduler.step()
+
+        syn_data, syn_labels = synthesize_data_with_uniform_labels(model, device, gen_batch_size=batch_size,
+                                                                   n_data=data_pkg.n_data,
+                                                                   n_labels=data_pkg.n_labels)
+        # print('size of syn_data', syn_data.shape)
+        # syn_data = (syn_data - np.mean(syn_data)) / np.std(syn_data)
+        # syn_data = syn_data * mnist_sdev + mnist_mean
+        #
+        # print('mean of syn_data', np.mean(syn_data))
+        # print('std of syn_data', np.std(syn_data))
+
+        dir_syn_data = log_dir + data_name + '/synthetic_mnist'
+        if not os.path.exists(dir_syn_data):
+            os.makedirs(dir_syn_data)
+
+        np.savez(dir_syn_data, data=syn_data, labels=syn_labels)
+        final_score = test_gen_data(log_dir2 + data_name, data_name, subsample=0.1, custom_keys='logistic_reg')
+        print('on logistic regression, accuracy is', final_score)
 
 
     # evaluating synthetic data on a classifier
@@ -122,22 +173,22 @@ def main():
     # elif data_name == 'fmnist':
     #     data_name = 'fashion'
 
-    syn_data, syn_labels = synthesize_data_with_uniform_labels(model, device, gen_batch_size=batch_size,
-                                                               n_data=data_pkg.n_data,
-                                                               n_labels=data_pkg.n_labels)
-
-    dir_syn_data = log_dir + data_name + '/synthetic_mnist'
-    if not os.path.exists(dir_syn_data):
-        os.makedirs(dir_syn_data)
-
-    np.savez(dir_syn_data, data=syn_data, labels=syn_labels)
-    # data_tuple = datasets_colletion_def(syn_data, syn_labels,
-    #                                     data_pkg.train_data.data, data_pkg.train_data.targets,
-    #                                     data_pkg.test_data.data, data_pkg.test_data.targets)
+    # syn_data, syn_labels = synthesize_data_with_uniform_labels(model, device, gen_batch_size=batch_size,
+    #                                                            n_data=data_pkg.n_data,
+    #                                                            n_labels=data_pkg.n_labels)
     #
-    # # test_results(data_name, data_name, 'log_dir', data_tuple, data_pkg.eval_func, 'logistic_reg')
-    final_score = test_gen_data(log_dir2 + data_name, data_name, subsample=1.0, custom_keys='logistic_reg')
-    print('on logistic regression, accuracy is', final_score)
+    # dir_syn_data = log_dir + data_name + '/synthetic_mnist'
+    # if not os.path.exists(dir_syn_data):
+    #     os.makedirs(dir_syn_data)
+    #
+    # np.savez(dir_syn_data, data=syn_data, labels=syn_labels)
+    # # data_tuple = datasets_colletion_def(syn_data, syn_labels,
+    # #                                     data_pkg.train_data.data, data_pkg.train_data.targets,
+    # #                                     data_pkg.test_data.data, data_pkg.test_data.targets)
+    # #
+    # # # test_results(data_name, data_name, 'log_dir', data_tuple, data_pkg.eval_func, 'logistic_reg')
+    # final_score = test_gen_data(log_dir2 + data_name, data_name, subsample=0.1, custom_keys='logistic_reg')
+    # print('on logistic regression, accuracy is', final_score)
 
 if __name__ == '__main__':
     main()
