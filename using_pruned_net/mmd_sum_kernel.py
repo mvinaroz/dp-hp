4c864e1e77e4606afaa3bd59d99673c8f86aec27
MijungTheGatsbyPostdoc <brients@gmail.com>
2021-03-03 11:06:33 +0100

    unbiased MMD estimator

--- using_pruned_net/mmd_sum_kernel.py
+++ using_pruned_net/mmd_sum_kernel.py
@@ -6,15 +6,24 @@ def mmd_loss(data_enc, data_labels, gen_enc, gen_labels, n_labels, sigma2, metho
  # set gen labels to scalars from one-hot
  _, gen_labels = pt.max(gen_labels, dim=1)
  batch_size = data_enc.shape[0]
+ feature_dim = data_enc.shape[1]
  mmd_sum = 0
+ mmd_sum1 = 0
  for idx in range(n_labels):
   idx_data_enc = data_enc[data_labels == idx]
   idx_gen_enc = gen_enc[gen_labels == idx]
+  # m = idx_data_enc.shape[0]
+  # n = idx_gen_enc.shape[0]
   if method=='sum_kernel':
-     mmd_sum += mmd_per_class(idx_data_enc, idx_gen_enc, pt.sqrt(sigma2))
+     mmd_sum += mmd_per_class(idx_data_enc, idx_gen_enc, pt.sqrt(sigma2), batch_size)
+
+     # for dim in np.arange(0, feature_dim):
+     #  print('dimension', dim)
+     #  dxx, dxy, dyy = get_squared_dist(idx_data_enc[:, dim].unsqueeze(1), idx_gen_enc[:, dim].unsqueeze(1))
+     #  mmd_sum1 += mmd_g(dxx, dxy, dyy, pt.sqrt(sigma2), batch_size)
   else:
      dxx, dxy, dyy = get_squared_dist(idx_data_enc, idx_gen_enc)
-     mmd_sum += mmd_g(dxx, dxy, dyy, batch_size, sigma=pt.sqrt(sigma2))
+     mmd_sum += mmd_g(dxx, dxy, dyy, pt.sqrt(sigma2), batch_size)
  return mmd_sum
 
 
@@ -41,7 +50,7 @@ def get_squared_dist(x, y=None):
  return dist_xx, dist_xy, dist_yy
 
 
-def mmd_per_class(x, y, sigma2):
+def mmd_per_class(x, y, sigma2, batch_size):
  """
  size(x) = mini_batch by feature_dimension
  size(y) = mini_batch by feature_dimension
@@ -52,22 +61,10 @@ def mmd_per_class(x, y, sigma2):
 
  m,feat_dim = x.shape
  n = y.shape[0]
- if m<1:
-  m_div = 1e-3
- else:
-  m_div = m
- if n<1:
-  n_div = 1e-3
- else:
-  n_div = n
 
- xx = pt.einsum('id,dj -> ijd', x, x.t()) # m by m by feature_dimension
- yy = pt.einsum('id,dj -> ijd', y, y.t()) # n by n by feature_dimension
- xy = pt.einsum('id,dj -> ijd', x, y.t()) #  m by n by feature_dimension
-
- # print('shape of xx', xx.shape)
- # print('shape of yy', yy.shape)
- # print('shape of xy', xy.shape)
+ xx = pt.einsum('id,jd -> ijd', x, x) # m by m by feature_dimension
+ yy = pt.einsum('id,jd -> ijd', y, y) # n by n by feature_dimension
+ xy = pt.einsum('id,jd -> ijd', x, y) #  m by n by feature_dimension
 
  x2 = x**2 # m by feat_dim
  x2_extra_dim1 = x2[:,None,:]
@@ -76,74 +73,33 @@ def mmd_per_class(x, y, sigma2):
  y2_extra_dim1 = y2[:,None,:]
  y2_extra_dim2 = y2[None,:,:]
 
- # print('shape of x2', x2.shape)
- # print('shape of y2', y2.shape)
-
  # first term: sum_d sum_i sum_j k(x_i, x_j)
- dist_xx = pt.abs(x2_extra_dim1.repeat(1,m,1) - 2.0*xx + x2_extra_dim2.repeat(m,1,1))
- kxx = pt.sum(pt.exp(-dist_xx/(2.0*sigma2**2)))/(m_div**2)/feat_dim
+ dist_xx = pt.nn.functional.relu(x2_extra_dim1.repeat(1,m,1) - 2.0*xx + x2_extra_dim2.repeat(m,1,1))
+ kxx = pt.sum(pt.exp(-dist_xx/(2.0*sigma2**2)))/(batch_size**2)
 
  # second term: sum_d sum_i sum_j k(y_i, y_j)
- dist_yy = pt.abs(y2_extra_dim1.repeat(1,n,1) - 2.0*yy + y2_extra_dim2.repeat(n,1,1))
- kyy = pt.sum(pt.exp(-dist_yy/(2.0*sigma2**2)))/(n_div**2)/feat_dim
+ dist_yy = pt.nn.functional.relu(y2_extra_dim1.repeat(1,n,1) - 2.0*yy + y2_extra_dim2.repeat(n,1,1))
+ kyy = pt.sum(pt.exp(-dist_yy/(2.0*sigma2**2)))/(batch_size**2)
 
  # third term: sum_d sum_i sum_j k(x_i, y_j)
- dist_xy = pt.abs(x2_extra_dim1.repeat(1,n,1) - 2.0*xy + y2_extra_dim2.repeat(m,1,1))
- kxy = pt.sum(pt.exp(-dist_xy/(2.0*sigma2**2)))/(m_div*n_div)/feat_dim
+ dist_xy = pt.nn.functional.relu(x2_extra_dim1.repeat(1,n,1) - 2.0*xy + y2_extra_dim2.repeat(m,1,1))
+ kxy = pt.sum(pt.exp(-dist_xy/(2.0*sigma2**2)))/(batch_size*batch_size)
 
  mmd = kxx + kyy - 2.0*kxy
 
  return mmd
 
+def mmd_g(dist_xx, dist_xy, dist_yy, sigma, batch_size):
 
-def mmd_g(dist_xx, dist_xy, dist_yy, batch_size, sigma, upper_bound=None, lower_bound=None):
- """This function calculates the maximum mean discrepancy with Gaussian distribution kernel
- The kernel is taken from following paper:
- Li, C.-L., Chang, W.-C., Cheng, Y., Yang, Y., & Póczos, B. (2017).
- MMD GAN: Towards Deeper Understanding of Moment Matching Network.
- :param dist_xx:
- :param dist_xy:
- :param dist_yy:
- :param batch_size:
- :param sigma:
- :param upper_bound: bounds for pairwise distance in mmd-g.
- :param lower_bound:
- :return:
- """
+ # We compute the biased MMD estimator
+ k_xx = pt.exp(-dist_xx / (2.0 * sigma ** 2))
+ k_yy = pt.exp(-dist_yy / (2.0 * sigma ** 2))
+ k_xy = pt.exp(-dist_xy / (2.0 * sigma ** 2))
 
- if lower_bound is None:
-  k_xx = pt.exp(-dist_xx / (2.0 * sigma ** 2))
-  k_yy = pt.exp(-dist_yy / (2.0 * sigma ** 2))
- else:
-  k_xx = pt.exp(-pt.max(dist_xx, lower_bound) / (2.0 * sigma ** 2))
-  k_yy = pt.exp(-pt.max(dist_yy, lower_bound) / (2.0 * sigma ** 2))
-
- if upper_bound is None:
-  k_xy = pt.exp(-dist_xy / (2.0 * sigma ** 2))
- else:
-  k_xy = pt.exp(-pt.min(dist_xy, upper_bound) / (2.0 * sigma ** 2))
-
- # m = tf.constant(batch_size, tf.float32)
- e_kxx = matrix_mean_wo_diagonal(k_xx, batch_size)
- e_kxy = matrix_mean_wo_diagonal(k_xy, batch_size)
- e_kyy = matrix_mean_wo_diagonal(k_yy, batch_size)
+ e_kxx = pt.sum(k_xx)/(batch_size**2)
+ e_kyy = pt.sum(k_yy)/(batch_size**2)
+ e_kxy = pt.sum(k_xy)/(batch_size**2)
 
  mmd = e_kxx + e_kyy - 2.0 * e_kxy
  return mmd
 
-
-def matrix_mean_wo_diagonal(matrix, num_row, num_col=None):
- """ This function calculates the mean of the matrix elements not in the diagonal
- 2018.4.9 - replace tf.diag_part with tf.matrix_diag_part
- tf.matrix_diag_part can be used for rectangle matrix while tf.diag_part can only be used for square matrix
- :param matrix:
- :param num_row:
- :type num_row: float
- :param num_col:
- :type num_col: float
- :param name:
- :return:
- """
- diff = pt.sum(matrix) - pt.sum((matrix.diag()))
- normalizer = num_row * (num_row - 1.0) if num_col is None else (num_row * num_col - min(num_col, num_row))
- return diff / normalizer
--- using_pruned_net/test_sum_kernel.py
+++ using_pruned_net/test_sum_kernel.py
@@ -4,13 +4,16 @@
 import torch
 import numpy as np
 import os
-# from torch.optim.lr_scheduler import StepLR
+from torch.optim.lr_scheduler import StepLR
 from all_aux_files import FCCondGen, ConvCondGen, meddistance, flatten_features, get_mnist_dataloaders, log_gen_data
 from all_aux_files import synthesize_data_with_uniform_labels, test_gen_data
 # test_passed_gen_data, datasets_colletion_def, test_results
 from mmd_sum_kernel import mmd_loss
 from all_aux_files import eval_hermite_pytorch
 from collections import namedtuple
+from torch.utils.data import DataLoader
+import torchvision.transforms as transforms
+from torchvision import datasets
 
 train_data_tuple_def = namedtuple('train_data_tuple', ['train_loader', 'test_loader',
                                                        'train_data', 'test_data',
@@ -30,84 +33,24 @@ def get_dataloaders(dataset_key, batch_size, test_batch_size, use_cuda, normaliz
 
   return train_data_tuple_def(train_loader, test_loader, trn_data, tst_data, n_features, n_data, n_labels, eval_func)
 
-def find_order(rho,eigen_val_threshold):
-    k = 100
-    eigen_vals = (1 - rho) * (rho ** np.arange(0, k + 1))
-    idx_keep = eigen_vals > eigen_val_threshold
-    keep_eigen_vals = eigen_vals[idx_keep]
-    print('keep_eigen_vals are ', keep_eigen_vals)
-    order = len(keep_eigen_vals)
-    print('The number of orders for Hermite Polynomials is', order)
-    return order
+def load_data(data_name, batch_size):
+    transform_digits = transforms.Compose([transforms.ToTensor(), transforms.Normalize([0.1307], [0.3081])])
+    transform_fashion = transforms.Compose([transforms.ToTensor()])
 
-def find_rho(sigma2):
-    alpha = 1 / (2.0 * sigma2)
-    # from this: alpha = rho / (1- rho**2), identify what rho is
-    # sol = optimize.minimize_scalar(lambda r: (alpha - r / (1 - r ** 2)) ** 2, bounds=(0, 1),
-    #                                method='bounded')
-    # rho = sol.x
-    # print('rho from optimization', rho)
-    rho = -1 / 2 / alpha + np.sqrt(1 / alpha ** 2 + 4) / 2
-    # print('rho from closed form solution', rho)
-    return rho
+    if data_name == 'digits':
+        train_dataset = datasets.MNIST(root='data', train=True, transform=transform_digits, download=True)
+    elif data_name == 'fashion':
+        train_dataset = datasets.FashionMNIST(root='data', train=True, transform=transform_fashion, download=True)
 
-
-def ME_with_HP(x, order, rho, device):
-    n_data, input_dim = x.shape
-    phi_x_mat = []
-    for axis in torch.arange(input_dim):
-        # print(axis)
-        x_axis = x[:, axis]
-        x_axis = x_axis[:, None]
-        phi_x_axis, eigen_vals_axis = feature_map_HP(order, x_axis, torch.tensor(rho), device)
-        me = torch.mean(phi_x_axis,axis=0)
-        phi_x_mat.append(me[:,None])
-
-    return phi_x_mat
-
-
-def feature_map_HP(k, x, rho, device):
-    # k: degree of polynomial
-    # rho: a parameter (related to length parameter)
-    # x: where to evaluate the function at
-    print('device', device)
-    eigen_vals = (1 - rho) * (rho ** torch.arange(0, k + 1))
-    print('eigen_vals', eigen_vals)
-    eigen_funcs_x = eigen_func(k, rho, x, device)  # output dim: number of datapoints by number of degree
-    print('eigen_funcs', eigen_funcs_x)
-
-    phi_x = torch.einsum('ij,j-> ij', eigen_funcs_x, torch.sqrt(eigen_vals))  # number of datapoints by order
-    # n_data = eigen_funcs_x.shape[0]
-    # mean_phi_x = torch.sum(phi_x,0)/n_data
-
-    return phi_x, eigen_vals
-
-
-def eigen_func(k, rho, x, device):
-    # k: degree of polynomial
-    # rho: a parameter (related to length parameter)
-    # x: where to evaluate the function at, size: number of data points by input_dimension
-    print('device', device)
-    orders = torch.arange(0, k + 1, device=device)
-    H_k = eval_hermite_pytorch(x, k + 1, device, return_only_last_term=False)
-    H_k = H_k[:, :, 0]
-    # H_k = eval_hermite(orders, x)  # input arguments: degree, where to evaluate at.
-    # output dim: number of datapoints by number of degree
-    rho = torch.tensor(rho, dtype=float, device=device)
-    # print('Device of Rho is ', rho.device, 'and device of x is ', x.device)
-    exp_trm = torch.exp(-rho / (1 + rho) * (x ** 2))  # output dim: number of datapoints by 1
-    N_k = (2 ** orders) * ((orders + 1).to(torch.float).lgamma().exp()) * torch.sqrt(((1 - rho) / (1 + rho)))
-    eigen_funcs = 1 / torch.sqrt(N_k) * (H_k * exp_trm)  # output dim: number of datapoints by number of degree
-    return eigen_funcs
-
+    train_loader = DataLoader(dataset=train_dataset,batch_size=batch_size,num_workers=0,shuffle=True)
+    return train_loader
 
 def main():
 
     data_name = 'digits' # 'digits' or 'fashion'
     method = 'sum_kernel' # sum_kernel or a_Gaussian_kernel
     """ this code only works for MMDest, for ME with HP, use ME_sum_kernel.py"""
-    loss_type = 'MMDest' # ME (mean-embedding with HP) or MMDest (MMD estimate)
-    single_release = True
+    loss_type = 'MMDest'
     model_name = 'CNN' # CNN or FC
     report_intermidiate_result = False
     device = 'cuda' if torch.cuda.is_available() else 'cpu'
@@ -117,8 +60,11 @@ def main():
     test_batch_size = 200
     data_pkg = get_dataloaders(data_name, batch_size, test_batch_size, True, False, [], [])
     train_loader = data_pkg.train_loader
-    # test_loader = data_pkg.test_loader
-    n_train_data = data_pkg.n_data
+    # # test_loader = data_pkg.test_loader
+    # n_train_data = data_pkg.n_data
+
+    # train_loader = load_data(data_name, batch_size)
+    n_train_data = 60000
 
     """ Define a generator """
     input_size = 5 # dimension of z
@@ -129,10 +75,12 @@ def main():
         model = FCCondGen(input_size, '500,500', feature_dim, n_classes, use_sigmoid=False, batch_norm=True).to(device)
         # for MNIST, with FC, logistic accuracy 81 percent (20 epochs) for full MMD
         # for Fashion-MNIST, with FC, logistic accuracy 0.687 (20 epochs) for full MMD
+        # for MNIST, with FC, 0.7564 (sum kernel)
     elif model_name == 'CNN':
         model = ConvCondGen(input_size, '500,500', n_classes, '16,8', '5,5', use_sigmoid=True, batch_norm=True).to(device)
         # for MNIST, with CNN, logistic accuracy 0.78 (20 epochs) for full MMD
         # for Fashion-MNIST, with CNN, logistic accuracy 0.689 (20 epochs) for full MMD
+        # for MNIST, with CNN, 0.7563 (sum kernel)
 
     """ Training """
     # set the scale length
@@ -173,20 +121,7 @@ def main():
         os.makedirs(log_dir)
 
     optimizer = torch.optim.Adam(list(model.parameters()), lr=0.001)
-    # scheduler = StepLR(optimizer, step_size=1, gamma=0.9)
-
-    if loss_type=='ME':
-        rho = find_rho(sigma2)
-        ev_thr = 1e-3  # eigen value threshold, below this, we wont consider for approximation
-        order = find_order(rho, ev_thr)
-        if single_release:
-            data_embedding = torch.zeros((feature_dim*order, n_classes))
-            for batch_idx, (data, labels) in enumerate(data_pkg.train_loader):
-                data, labels = data.to(device), labels.to(device)
-                data = flatten_features(data)
-                for idx in range(n_classes):
-                    idx_data = data[labels == idx]
-                    data_embedding[:,idx] = 0.5*(data_embedding[:,idx] + ME_with_HP(idx_data,order,rho,device))
+    scheduler = StepLR(optimizer, step_size=1, gamma=0.9)
 
     for epoch in range(1, n_epochs + 1):
         model.train()
@@ -198,41 +133,21 @@ def main():
             gen_code, gen_labels = model.get_code(batch_size, device)
             gen_samples = model(gen_code) # batch_size by 784
 
-            if loss_type == 'MMDest':
-                loss = mmd_loss(data, labels, gen_samples, gen_labels, n_classes, sigma2, method)
-            elif loss_type == 'ME':
-                if single_release:
-                    synth_data_embedding = torch.zeros((feature_dim * order, n_classes))
-                    _, gen_labels_numerical = torch.max(gen_labels, dim=1)
-                    for idx in range(n_classes):
-                        idx_synth_data = gen_samples[gen_labels_numerical == idx]
-                        synth_data_embedding[:, idx] = 0.5 * (synth_data_embedding[:, idx] + ME_with_HP(idx_synth_data, order, rho, device))
-                else:
-                    synth_data_embedding = torch.zeros((feature_dim * order, n_classes))
-                    data_embedding = torch.zeros((feature_dim * order, n_classes))
-                    _, gen_labels_numerical = torch.max(gen_labels, dim=1)
-                    for idx in range(n_classes):
-                        idx_data = data[labels == idx]
-                        data_embedding[:, idx] = 0.5 * (data_embedding[:, idx] + ME_with_HP(idx_data, order, rho, device))
-                        idx_synth_data = gen_samples[gen_labels_numerical == idx]
-                        synth_data_embedding[:, idx] = 0.5 * (synth_data_embedding[:, idx] + ME_with_HP(idx_synth_data, order, rho, device))
-
-                loss = torch.mean((data_embedding - synth_data_embedding) ** 2)
-
+            loss = mmd_loss(data, labels, gen_samples, gen_labels, n_classes, sigma2, method)
             loss.backward()
             optimizer.step()
         # end for
 
         print('Train Epoch: {} [{}/{}]\tLoss: {:.6f}'.format(epoch, batch_idx * len(data), n_train_data, loss.item()))
 
-        log_gen_data(model, device, epoch, data_pkg.n_labels, log_dir)
-        # scheduler.step()
+        log_gen_data(model, device, epoch, n_classes, log_dir)
+        scheduler.step()
 
         if report_intermidiate_result:
             """ now we save synthetic data and test them on logistic regression """
             syn_data, syn_labels = synthesize_data_with_uniform_labels(model, device, gen_batch_size=batch_size,
-                                                                       n_data=data_pkg.n_data,
-                                                                       n_labels=data_pkg.n_labels)
+                                                                       n_data=n_train_data,
+                                                                       n_labels=n_classes)
 
             dir_syn_data = log_dir + data_name + '/synthetic_mnist'
             if not os.path.exists(dir_syn_data):
@@ -248,8 +163,8 @@ def main():
     #########################################################################3
     """ Once we have a trained generator, we store synthetic data from it and test them on logistic regression """
     syn_data, syn_labels = synthesize_data_with_uniform_labels(model, device, gen_batch_size=batch_size,
-                                                               n_data=data_pkg.n_data,
-                                                               n_labels=data_pkg.n_labels)
+                                                               n_data=n_train_data,
+                                                               n_labels=n_classes)
 
     dir_syn_data = log_dir + data_name + '/synthetic_mnist'
     if not os.path.exists(dir_syn_data):
