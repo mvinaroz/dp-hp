f53a1c23d2addfb053f2340c48b275642d95f661
mvinaroz <margarita.vinaroz@gmail.com>
2021-05-28 18:11:09 +0200

    removing more files

--- code_balanced/jan21_synth_2d.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-python gen_balanced.py --data 2d -noise 0. --synth-spec-string norm_k5_n50000_row5_col5_noise0.2 -ep 50 --log-name jan21_test1 --gen-spec 200,500,500,200 --rff-sigma 0.3 -lr 1e-3 --d-rff 400 --loss-type hermite --sampling-rate 0.7
